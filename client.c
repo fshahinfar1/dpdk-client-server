bba1acc7c72719b3bc94ffa03d74220931dc3e6e
Farbod Shahinfar <fshahinfar1@gmail.com>
2025-01-23 07:13:18 -0700

    Enable sending TCP requests with Katran server-id header option

--- client.c
+++ client.c
@@ -9,6 +9,7 @@
 #include <rte_ip.h>
 #include <rte_mbuf.h>
 #include <rte_udp.h>
+#include <rte_tcp.h>
 // current project
 #include "exp.h"
 #include "percentile.h"
@@ -19,6 +20,17 @@
 #define BURST_SIZE (512)
 #define MAX_EXPECTED_LATENCY (10000) // (us)
 #define PAYLOAD "rtqeijsuiggqlxkuuvsoerdzvgbphgpyrkecwbsynngpruiubtgzcwtfltjmmnpwapcbyioboiqdbxebcrqyehebezbdwyrvdbhxfsbearajfmmscsinujutdqcftxchgzptyfypojbmnpjovoartkwupbfowfvxhfimrltocjoousmumwrqvjxukjtztcyahxfoldflyquyixahobffjawyzzaawghbjgfhpajqevfflpgoiiotkqjbajhhvyhmnydmkxbrgpbavcdaanjopmnsewoebkqgqcbvxsblfgulcogxeqkaxnytevmpwobljlxtjhygawmbhktewhbiytjlmjxxtfmwytlogigvpfsgihyxgkmskppxikspqmnrarxbzihzwqufsltxiioyvcsrhjiqlcfqcavtxsrbqnogyxwerqlpiwextpuxvmflwbazjynzeiprcttniqtmdusjxwqfdzfocowaywwnmedqjfizajdqbetslgjqzsvadscxbdwrywffiwlgybupukobjpjlspaofjkmhszxzskirdieshmpfqsggjnhyiiadaumiisuontoomswlyhiyuwaupjwfjdeulymoqvmrwlncncqdaobnfmbiknxwadgbrpsfixqhnbbgnacuoivmlyxzflqnoobpqffnmkpxkzcyvoqnjvibphoxueqkjqhvgjurnxchlbncxvvbeettppluoxtzewefcaktcupcqueeymcyietauqtgycceyhfqpawsnydcuehnjfpkpnsvmpieauhoawchmpiymzirhvfxcnrtdsgmxoblqycfzfsgzvtdwdhcqtakezphfntfsxagkrtwofqsaipibbzqydlcvlungidzgqmkkreznoutrclipksaaefpokubbycpkpfddceydjxjmdpryujwqqpeohbmyhwrgeqtsirfykynkelarbjdfycjzjfuzcitiaadbfvwlwteefdsqvzarxhrtbsjeppkpszjrdfvkufynwvihygfykpvitpaqnxdedkytwlkcbttogfaqdsveqrtymflcxhvbumcifjklzpcqhfbpbwhyoaekjpcisswegubzcyjdwzyqxsshkpdsynfofowakfmasnudmzazgdxvvtajatwxskuitxjoewuvbhhhjdhqozoqfvquyioizhlqwqyaidvokcfbcsgnhfmthsmktbntvoleaeznatmmuawslyinilsvefdizgnbnayaxhzxxphuyzyfycmrsmidqlglknsxchkqstcsqaofvxgscfqsqlfvcwvbnhrxdclzxwettbkpsfyaafowvhlgmglesaehsionovcshwkxfxtaczcxpgzevwrbclqtkfhfbuztqhcylyufuhbcjodlxyssvvikfalxdxadvllbhyxelqsadeuxpjnochcxdlgxgjzderdhnwahmuzbqtwpnsuwhhxfwcbuahkgctljjqvqct"
+
+#define SEND_TCP_WITH_KATRAN_SERVER_OPT 1
+#ifdef SEND_TCP_WITH_KATRAN_SERVER_OPT
+// the structure of the header-option used to embed server_id is:
+//  __u8 kind | __u8 len | __u32 server_id
+// Arbitrarily picked unused value from IANA TCP Option Kind Numbers
+#define KATRAN_TCP_HDR_OPT_KIND_TPR 0xB7
+// Length of the tcp header option
+#define KATRAN_TCP_HDR_OPT_LEN_TPR 6
+#define KATRAN_MAX_QUIC_REALS 0x00fffffe
+#endif
 
 // function declarations
 void *_run_receiver_thread(void *_arg);
@@ -351,55 +363,79 @@ int do_client(void *_cntx) {
         ipv4_hdr = (struct rte_ipv4_hdr *)buf_ptr;
         ipv4_hdr->version_ihl = 0x45;
         ipv4_hdr->type_of_service = 0;
-        ipv4_hdr->total_length =
-            rte_cpu_to_be_16(sizeof(struct rte_ipv4_hdr) +
-                             sizeof(struct rte_udp_hdr) + payload_length);
         ipv4_hdr->packet_id = 0;
         ipv4_hdr->fragment_offset = 0;
         ipv4_hdr->time_to_live = 64;
-        ipv4_hdr->next_proto_id = IPPROTO_UDP;
         ipv4_hdr->hdr_checksum = 0;
 
         const uint32_t src_idx = src_zipf->gen(src_zipf) - 1;
         const uint32_t ip_offset = src_idx / src_id_count_ports;
         const uint32_t port_offset = src_idx % src_id_count_ports;
         ipv4_hdr->src_addr = rte_cpu_to_be_32(src_ip + ip_offset);
-        /* static unsigned short y = 0; */
-        /* unsigned int _ip = src_ip + y; */
-        /* ipv4_hdr->src_addr = rte_cpu_to_be_32(_ip); */
-
         ipv4_hdr->dst_addr = rte_cpu_to_be_32(dst_ip);
 
+#ifndef SEND_TCP_WITH_KATRAN_SERVER_OPT
+        ipv4_hdr->next_proto_id = IPPROTO_UDP;
+        ipv4_hdr->total_length =
+            rte_cpu_to_be_16(sizeof(struct rte_ipv4_hdr) +
+                             sizeof(struct rte_udp_hdr) + payload_length);
         // upd header
-        buf_ptr = rte_pktmbuf_append(buf, sizeof(struct rte_udp_hdr) +
-                                              payload_length);
+        buf_ptr = rte_pktmbuf_append(buf, sizeof(struct rte_udp_hdr));
         udp_hdr = (struct rte_udp_hdr *)buf_ptr;
-
-        /* static unsigned short f = 0; */
-        /* unsigned short port  = 1000 + f; */
-        /* f = (f+1) % 15000; */
-        /* if (f == 0) y = (y + 1) % 15000; */
-        /* udp_hdr->src_port = rte_cpu_to_be_16(port); */
         udp_hdr->src_port = rte_cpu_to_be_16(src_port + port_offset);
-
         udp_hdr->dst_port = rte_cpu_to_be_16(dst_port);
-        udp_hdr->dgram_len = rte_cpu_to_be_16(sizeof(struct rte_udp_hdr) + payload_length);
+        const size_t dgram_len = sizeof(struct rte_udp_hdr) + payload_length;
+        udp_hdr->dgram_len = rte_cpu_to_be_16(dgram_len);
         udp_hdr->dgram_cksum = 0;
+#else
+        // the TCP option is 6 bytes and 2 bytes of padding for making it a multiple of 4B (32-bit block)
+        const size_t size_tcp_opts = 8;
+        const size_t hdr_size = sizeof(struct rte_tcp_hdr) + size_tcp_opts;
+        ipv4_hdr->next_proto_id = IPPROTO_TCP;
+        ipv4_hdr->total_length = rte_cpu_to_be_16(sizeof(struct rte_ipv4_hdr) +
+            hdr_size + payload_length);
+        buf_ptr = rte_pktmbuf_append(buf, sizeof(struct rte_tcp_hdr));
+        struct rte_tcp_hdr *tcp_hdr = (struct rte_tcp_hdr *)buf_ptr;
+        tcp_hdr->src_port = rte_cpu_to_be_16(src_port + port_offset);
+        tcp_hdr->dst_port = rte_cpu_to_be_16(dst_port);
+        tcp_hdr->sent_seq = 123; // TODO: does it matter in our experiment?
+        tcp_hdr->recv_ack = 321; // TODO: does it matter in our experiment?
+        tcp_hdr->data_off = (((hdr_size) / 4) & 0xf) << 4; // how many 32 bit rows?
+        tcp_hdr->tcp_flags = RTE_TCP_ACK_FLAG | RTE_TCP_PSH_FLAG;
+        tcp_hdr->rx_win = 256; // TODO: does it matter in our experiment?
+        tcp_hdr->cksum = 0;
+        tcp_hdr->tcp_urp = 0;
+        buf_ptr = rte_pktmbuf_append(buf, size_tcp_opts);
+        struct {
+          uint8_t kind;
+          uint8_t len;
+          uint32_t srv_id;
+          uint16_t pad;
+        } __attribute__((packed)) *opt  = (void *)buf_ptr;
+        opt->kind = KATRAN_TCP_HDR_OPT_KIND_TPR;
+        opt->len = KATRAN_TCP_HDR_OPT_LEN_TPR;
+        opt->srv_id = src_idx % KATRAN_MAX_QUIC_REALS;
+        if (opt->srv_id == 0) // server id zero is invalid
+          opt->srv_id = 1;
+        opt->pad = 0;
+        // TODO: do I need to add more options?
+#endif
 
         /* payload */
+        buf_ptr = rte_pktmbuf_append(buf, payload_length);
         /* add timestamp */
         // TODO: This timestamp is only valid on this machine, not time sycn.
         // maybe ntp or something similar should be implemented
         // or just send the base time stamp at the begining.
         timestamp = rte_get_timer_cycles();
-        *(uint64_t *)(buf_ptr + (sizeof(struct rte_udp_hdr))) = timestamp;
+        *(uint64_t *)buf_ptr = timestamp;
 
         /* request a fib number */
         /* *(uint64_t *)(buf_ptr + (sizeof(struct rte_udp_hdr))) = 0; */
         /* *(uint32_t *)(buf_ptr + (sizeof(struct rte_udp_hdr))) = 80; */
 
-        memcpy(buf_ptr + sizeof(struct rte_udp_hdr) + sizeof(timestamp),
-            PAYLOAD, payload_length - sizeof(timestamp));
+        memcpy(buf_ptr + sizeof(timestamp), PAYLOAD,
+            payload_length - sizeof(timestamp));
 
         if (use_vlan) {
           buf->l2_len = RTE_ETHER_HDR_LEN + sizeof(struct rte_vlan_hdr);
@@ -407,7 +443,13 @@ int do_client(void *_cntx) {
           buf->l2_len = RTE_ETHER_HDR_LEN;
         }
         buf->l3_len = sizeof(struct rte_ipv4_hdr);
+        // TODO: if the hardware does not support the checksum offload, I have
+        // to calculate it in this program ...
+#ifdef SEND_TCP_WITH_KATRAN_SERVER_OPT
+        buf->ol_flags = RTE_MBUF_F_TX_IP_CKSUM | RTE_MBUF_F_TX_IPV4 | RTE_MBUF_F_TX_TCP_CKSUM;
+#else
         buf->ol_flags = RTE_MBUF_F_TX_IP_CKSUM | RTE_MBUF_F_TX_IPV4 | RTE_MBUF_F_TX_UDP_CKSUM;
+#endif
       }
 
       /* send packets */
